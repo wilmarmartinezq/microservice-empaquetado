643747615dc3ef59f95b85ba6d5604af254daa87
wamartinezq <w.martinezq@uniandes.edu.co>
2023-03-15 18:21:38 -0500

    test

--- src/eda/modulos/ordenes/main.py
+++ src/eda/modulos/ordenes/main.py
@@ -67,6 +67,15 @@ async def suscribirse_a_topico(topico: str, suscripcion: str, schema: Record, ti
 
 
 
+class Despachador:
+    def __init__(self):
+        ...
+
+    def publicar_mensaje(self, mensaje, topico):
+        cliente = pulsar.Client(f'pulsar://{broker_host()}:6650')
+        publicador = cliente.create_producer(topico, schema=AvroSchema(mensaje.__class__))
+        publicador.send(mensaje)
+        cliente.close()
 
 
 def time_millis():
